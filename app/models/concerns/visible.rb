2ffe3ddcc766a2b260623a3e0445f441ad11bf2c
Lucius Choi <lucius.choi@gmail.com>
2023-08-05 11:45:00 +0900

    Concern에 클래스 메소드 추가하기

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -7,6 +7,12 @@ module Visible
     validates :status, inclusion: { in: %w[public private archived] }
   end
 
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
   end
