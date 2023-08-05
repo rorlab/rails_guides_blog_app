9a0a6dcd7322ae035aaf6e18ca8fda8325359d72
Lucius Choi <lucius.choi@gmail.com>
2023-08-05 11:20:03 +0900

    오타 수정

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true
-  validates :body, presence: true, length: { mininum: 10 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
